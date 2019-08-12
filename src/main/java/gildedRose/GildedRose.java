29eb4246d26ce7e95b2021745686f670fc63cca2
Tiger Zhu <tiger.zhu@oocl.com>
2019-08-12 20:13:13 +0800

    refactor extract the "calculateQualityBySellIn" function, Because it can calculate quality by Item's sellIn

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -24,29 +24,33 @@ public class GildedRose {
             }
 
 
-            getItemSellIn(items[i]);
+            calculateItemSellIn(items[i]);
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+            calculateQualityBySellIn(items[i]);
+        }
+    }
+
+    private void calculateQualityBySellIn(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
     }
 
-    private void getItemSellIn(Item item) {
+    private void calculateItemSellIn(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn = item.sellIn - 1;
         }

dbb5924676c3f397fd96b806b85472c7b489cec5
Tiger Zhu <tiger.zhu@oocl.com>
2019-08-12 19:49:26 +0800

    refactor extract the "calculateQualityForBackstage" function, Because Backstage has own function to calculate quality

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -20,19 +20,7 @@ public class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                    calculateQualityForBackstage(items[i]);
                 }
             }
 
@@ -55,6 +43,22 @@ public class GildedRose {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
+                }
+            }
+        }
+    }
+
+    private void calculateQualityForBackstage(Item item) {
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
